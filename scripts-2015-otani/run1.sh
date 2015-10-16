4c5be0a2d564ae36ac81d696ce166ff03d00e699
niwatoritowani <jkonishi@pnl-d74-1.bwh.harvard.edu>
2015-10-16 12:08:52 -0400

    update run1.sh

--- scripts-2015-otani/run1.sh
+++ scripts-2015-otani/run1.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -eu
 
-case=$1
 caselist=caselist_jun
-grep "$1" $caselist || echo "$1" >> $caselist
-tmp=$(date +%Y%m%d%H%M%S.%N)
-exec &> >(tee $1.log.${tmp})
+awk '{print $1}' $caselist | grep -w "$1" || echo "$1" >> $caselist
 
+tmp=$(date +%Y%m%d%H%M%S.%N)
+logfile=$1.log.${tmp}
+exec &> >(tee ${logfile})
+
+case=$1
 out=${case}
 cmd="
-rsync -av -e ssh \
-    jkonishi@170.223.221.158:/rfanfs/pnl-zorro/projects/mclean/filtered_spgr/${case}/${case}.nhdr \
-    jkonishi@170.223.221.158:/rfanfs/pnl-zorro/projects/mclean/filtered_spgr/${case}/${case}.raw.gz \
-    .
-fs.sh -i ${case}.nhdr -o /PHShome/jk318/2015-otani/fsoutputs -s &
+    rsync -auv -e ssh \\
+        jkonishi@170.223.221.158:/rfanfs/pnl-zorro/projects/mclean/filtered_spgr/${case}/${case}.nhdr \\
+        jkonishi@170.223.221.158:/rfanfs/pnl-zorro/projects/mclean/filtered_spgr/${case}/${case}.raw.gz \\
+        .
+    fs.sh -i ${case}.nhdr -o /PHShome/jk318/2015-otani/fsoutputs -s >> ${logfile} 2>&1 ; \\
+        echo \"\$(date)\" &
 "
-
-if [ -e $out ]; then 
+if [ -e $out ]; then
     echo "$out exist"
-else 
+    echo "$(date)"
+else
     echo "$cmd"
     eval "$cmd"
 fi
-echo "$(date)"
-echo "Done"
+

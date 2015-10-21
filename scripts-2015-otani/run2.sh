fe1003ad0fde188bd1aa394a982ca6d82397630f
niwatoritowani <jkonishi@pnl-d74-1.bwh.harvard.edu>
2015-10-21 18:08:45 -0400

    update

--- scripts-2015-otani/run2.sh
+++ scripts-2015-otani/run2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-caselist=caselist
+caselist=caselist_jun
 awk '{print $1}' $caselist | grep -w "$1" || echo "$1" >> $caselist
 
 tmp=$(date +%Y%m%d%H%M%S.%N)
@@ -11,13 +11,14 @@ case=$1
 out=${case}.t1atlasmask.nrrd
 cmd="
     command \
-        input t1w-realign trainingt1list trainingmasklist \
-        output probabilitymap \
+        input ${case}.t1w-realign trainingt1list trainingmasklist \
+        output ${case}.probabilitymap \
         >> ${logfile} 2>&1
     command \
-        input probabilitymap \
+        input ${case}.probabilitymap \
         output ${out} \
         >> ${logfile} 2>&1
+    rm ${case}.probabilitymap
 "
 if [ -e $out ]; then
     echo "$out exist"

fbd729fb6d8167de6c30db9e2e289d4b8fba2a06
niwatoritowani <niwatoritowani@users.noreply.github.com>
2015-11-02 15:38:32 -0500

    Create SetUpData_env_PNL.r

---
+++ 2014-freeesurfer/SetUpData_env_PNL.r
@@ -0,0 +1,14 @@
+# todo
+# - on editing
+
+library(xlsx)
+library(knitr)    # for kable
+library(ggplot2)
+library(gridExtra)
+library(car)    # for Anova()
+
+setwd("/projects/schiz/3Tprojects/")
+demographictable="/projects/schiz/3Tprojects/"
+fsstatfile.aseg="edited.aseg_stats.txt"
+fsstatfile.rh="edited.aparc_stats_rh_volume.txt"
+fsstatfile.lh="edited.aparc_stats_lh_volume.txt"

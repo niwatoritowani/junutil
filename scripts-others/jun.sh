64ef5f627a7dcc3eb80b336cfaaf17e4bb7b27af
niwatoritowani <niwatoritowani@users.noreply.github.com>
2015-11-03 17:00:03 -0500

    change commands to functions

--- scripts-others/jun.sh
+++ scripts-others/jun.sh
@@ -5,43 +5,60 @@
 #    cat [caselist] | while read line; do bash jun.sh $line [&]; done (bash)
 #    awk '{print $1}' [caselist] | while read line; do bash jun.sh $line [&]; done (bash)
 
-# -----------------------------------------------
-# add ${case} to caselist if it is not yet listed
-# -----------------------------------------------
-
-#set +e     # not needed?
-caselist=caselist
-awk '{print $1}' $caselist | grep -w "$1" || echo "$1" >> $caselist
-#set -e    # not needed?
-
-# ---------------------------------
-# set standard-output into log-file
-# ---------------------------------
-
-tmp=$(date +%Y%m%d%H%M%S.%N)
-logfile=$1.log.${tmp}    # in case it is mentioned in cmd
-# cp /dev/null ${logfile}    # if necessary, initialization 
-exec &> >(tee -a ${logfile}) # add output-1-and-2 into terminal and file
-# if without -a, output of >> ${logfile} comes after the exec output, Why?
-
-# ----------------------------------------------
-# echo and run commands if ${out} does not exist
-# ----------------------------------------------
+# set variables
 
 case=$1
+caselist=caselist
+
+# add ${case} to ${caselist} if it is not yet listed
+
+add2list() {
+    #set +e     # not needed?
+    awk '{print $1}' $caselist | grep -w "${case}" || echo "${case}}" >> $caselist
+    #set -e    # not needed?
+}
+
+# set standard-output into log-file
+
+startlog() {
+    tmp=$(date +%Y%m%d%H%M%S.%N)
+    logfile=$1.log.${tmp}    # in case it is mentioned in cmd
+    # cp /dev/null ${logfile}    # if necessary, initialization 
+    exec &> >(tee -a ${logfile}) # add output-1-and-2 into terminal and file
+    # if without -a, output of >> ${logfile} comes after the exec output, Why?
+}
+
+# echo and run commands if ${out} does not exist
+
+check_out() {
+    if [ -e $1 ]; then
+        echo "$1 exist."
+        echo "exit"
+        echo "$(date)"
+        exit 1
+}
+
+echoeval() {
+    echo "$*"
+    eval "$@"
+    echo "Done"
+    echo "$(date)"
+}
+
+# --------
+# execute
+# --------
+
+add2list
+startlog ${case}
 out=${case}/${case}.file
 cmd="
     echo \"hello\" > $out 
     ls >> ${logfile} 2>&1 # if output is long and you want to run background
         #  if without >> but >, overwrite logfile
 "
-if [ -e $out ]; then
-    echo "$out exist"
-else
-    echo "$cmd"
-    eval "$cmd"
-fi
-echo "$(date)"
+check_out "${out}"
+echoeval "$cmd"
 
 # --------------------------------------------------------------------------------------
 # in the cmd some special characters are expanded by shell, then will be expanded again in eval

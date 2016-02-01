62ee45eefb0d1963de81019b7dd5d7159a6fb459
Jun Konishi <jkonishi@pnl-d74-1.bwh.harvard.edu>
2016-02-01 13:53:26 -0500

    change the path of demographictable

--- scripts-r/SetUpData_env_PNL.r
+++ scripts-r/SetUpData_env_PNL.r
@@ -6,7 +6,8 @@ library(gridExtra)
 library(car)    # for Anova()
 
 setwd("/projects/schiz/3Tprojects/2015-jun-prodrome/stats/02_editedfreesurfer")
-demographictable="/projects/schiz/3Tprojects/2015-jun-prodrome/caselist/Caselist_CC_prodromes.xlsx"
+#demographictable="/projects/schiz/3Tprojects/2015-jun-prodrome/caselist/Caselist_CC_prodromes.xlsx"
+demographictable="/rfanfs/pnl-zorro/Collaborators/jun/2015-jun-prodrome/caselist/Caselist_CC_prodromes.xlsx"
 fsstatfile.aseg="edited.aseg_stats.txt"
 fsstatfile.rh="edited.aparc_stats_rh_volume.txt"
 fsstatfile.lh="edited.aparc_stats_lh_volume.txt"

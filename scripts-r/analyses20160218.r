bd930542b08681d9525e42ead00891f522f2f401
niwatoritowani <niwatoritowani@example.com>
2016-02-18 14:14:18 -0500

    plots for correlation
    plots for correlation were added
    some changes in the plots for volume analyses were done

--- scripts-r/analyses20160218.r
+++ scripts-r/analyses20160218.r
@@ -3,18 +3,14 @@
 # ------------------------------
 
 # - plots of volumes between groups
-# - 
-# - 
-# - 
+# - plots of correlation
 
 
 # -----------------------------------------------
 # to do
 # -----------------------------------------------
 
-# - change axis labels
 # - change transparency of the dots in the plots
-# - graphs for correlations
 
 
 # -----------------------------------------------
@@ -38,33 +34,13 @@ field.names=c("Right.Hippocampus","Left.Hippocampus")
 data.hip=jun.stack(field.names,c("caseid2","GROUP","ICV"))
 
 
-
-# CCs, LV, LVThorn
-
-# # change the order of the levels in factor
-# data.cc$Regions2=factor(data.cc$Region2,regions)    # the order of the levels to be the same as in regions
-# data.cc$Diagnosis2=factor(data.cc$Diagnosis2,rev(levels(data.cc$Diagnosis2)))
-# n=length(levels(data.cc$Regions2));m=length(levels(data.cc$Diagnosis2))
-# data.cc$R2D2=factor(
-#     paste(data.cc$Region2,data.cc$Diagnosis2,sep="."),
-#     paste(levels(data.cc$Region2)[sort(rep(1:n,m))],levels(data.cc$Diagnosis2)[rep(1:m,n)],sep=".")
-# )
-# 
-# datax=data.cc
-# p1=ggplot(datax, aes(x=R2D2,y=Vol2,fill=Diagnosis2)) +
-#     scale_fill_manual(values=c("red","blue")) +
-#     geom_dotplot(binaxis="y",stackdir="center") +
-#     stat_summary(fun.y="mean",goem="point",shape=23,size=0.5,fill="black",ymin=0,ymax=0) +
-# #    guides(fill=FALSE) +    # don't display guide
-#     theme(axis.title.x=element_blank())    # don't display x-axis-label
-
-
 # function to plot
+# - refer to analyses20151201.r
 
-jun.plot2 <- function(data.cc) {
+jun.plot2 <- function(data.cc,ylabel1) {
     # change the order of the levels in factor
-    data.cc$Regions2=factor(data.cc$ind)     # the order of the leves are not changed
-    data.cc$Diagnosis2=factor(data.cc$GROUP,rev(levels(data.cc$GROUP)))    # lever order to be PRO, HVPRO
+    data.cc$ind=factor(data.cc$ind)     # the order of the leves are not changed
+    data.cc$GROUP=factor(data.cc$GROUP,rev(levels(data.cc$GROUP)))    # lever order to be PRO, HVPRO
     n=length(levels(data.cc$GROUP));m=length(levels(data.cc$GROUP))
     data.cc$R2D2=factor(
         paste(data.cc$ind,data.cc$GROUP,sep="."),
@@ -72,23 +48,27 @@ jun.plot2 <- function(data.cc) {
     )
 
     datax=data.cc
+    ylabel=paste("Volumes of",ylabel1)    # combine tests for y label
     p1=ggplot(datax, aes(x=GROUP,y=values,fill=ind)) +
         scale_fill_manual(values=c("red","blue")) +
         geom_dotplot(binaxis="y",stackdir="center") +
         stat_summary(fun.y="mean",goem="point",shape=23,size=0.5,fill="black",ymin=0,ymax=0) +
         guides(fill=FALSE) +    # don't display guide
-        theme(axis.title.x=element_blank())    # don't display x-axis-label
+        theme(axis.title.x=element_blank()) +    # don't display x-axis-label : GROUP
+        scale_x_discrete(breaks=c("HVPRO","PRO"),
+            labels=c("HC","CHR")) +   # change the labels of the scale
+        ylab(ylabel)    # change the label of y axis
     p1
 }
 
 
 # make plot componets
 
-p1=jun.plot2(data.cc.central)
-p2=jun.plot2(data.lv)
-p3=jun.plot2(data.lvt)
-p4=jun.plot2(data.amy)
-p5=jun.plot2(data.hip)
+p1=jun.plot2(data.cc.central,"CCC")
+p2=jun.plot2(data.lv,"LV")
+p3=jun.plot2(data.lvt,"temporal horns of LV")
+p4=jun.plot2(data.amy,"amygdala")
+p5=jun.plot2(data.hip,"hippocampus")
 
 
 # display in one picture
@@ -106,5 +86,39 @@ print(p2,vp=define_region(2,1))
 print(p3,vp=define_region(3,1))
 print(p4,vp=define_region(4,1))
 print(p5,vp=define_region(5,1))
+
+
+
+
+# -----------------------------------------------
+# plots of correlation
+# -----------------------------------------------
+
+
+# set up data
+
+datax=data.ex3.exna
+data.pro=subset(datax,GROUP=="PRO")
+data.hc=subset(datax,GROUP=="HVPRO")
+
+
+# plot
+
+datax=data.pro
+p1=ggplot(datax, aes(x=r.Bil.Lateral.Ventricle, y=r.Bil.Inf.Lat.Vent)) +
+    geom_point() + stat_smooth(method=lm, se=FALSE) +
+    xlab("Volumes of LV") +
+    ylab("Volumes of tenporal horns of LV")
+p2=ggplot(datax, aes(x=SINTOTEV, y=r.Bil.Lateral.Ventricle)) +
+    geom_point() + stat_smooth(method=lm, se=FALSE) +
+    xlab("Negative symptom score") + 
+    ylab("Volumes of LV")
+p3=ggplot(datax, aes(x=SIPTOTEV, y=r.Bil.Hippocampus)) +
+    geom_point() + stat_smooth(method=lm, se=FALSE) +
+    xlab("Positive symptom score") +
+    ylab("Volumes of hippocampus")
+grid.arrange(p1, p2, p3, main = "Correlation")
+
+
 
 

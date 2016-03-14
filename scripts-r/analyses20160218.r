b16592f5b92341f507a93f267431c03653b8e3e6
niwatoritowani <niwatoritowani@example.com>
2016-03-14 19:29:56 -0400

    update

--- scripts-r/analyses20160218.r
+++ scripts-r/analyses20160218.r
@@ -263,29 +263,139 @@ p1=jun.plot31(data.cc.central,"\nthe CCC",xlabel1,"A",c(1.05,1.1),c(1,1))
 xlabel1=c("left","right","left","right")
 p3=jun.plot31(data.lvt,"\nthe temporal horns of the LV",xlabel1,"B",c(0.05,1.1),c(0,1))
 p4=jun.plot31(data.amy,"\nthe amygdala",xlabel1,"C",c(1.05,1.1),c(1,1)) 
+options(scipen=2) # default: options(scipen=0)
 grid.arrange(p1, p3, p4)
+options(scipen=0) # default
 
 dev.copy2eps(file="plot_vol_group_20160311.eps",width=3.3, height=6)  # inches
 
 ppi=600    # 300 is better
 tiff("plot_vol_group_20160310.tif",width=3*ppi, height=6*ppi, res=ppi)
+options(scipen=2) # default: options(scipen=0)
 grid.arrange(p1, p3, p4)
+options(scipen=0) # default
 dev.off()
 
-# ppi=300    # 300 is better
-# png("plot_vol_group_CCC_20160309.png",width=3*ppi, height=2.5*ppi, res=ppi)
-# p1
-# dev.off()
-# 
-# ppi=300    # 300 is better
-# png("plot_vol_group_TH_20160309.png",width=3*ppi, height=2.5*ppi, res=ppi)
-# p3
-# dev.off()
-# 
-# ppi=300    # 300 is better
-# png("plot_vol_group_AMY_20160309.png",width=3*ppi, height=2.5*ppi, res=ppi)
-# p4
-# dev.off()
+# for poster
+
+ppi=300    # 300 is better
+png("plot_vol_group_CCC_20160313_300dpi_w3.3inch.png",width=3.3*ppi, height=2.5*ppi, res=ppi)
+p1
+dev.off()
+
+ppi=300    # 300 is better
+png("plot_vol_group_TH_20160313_300dpi_w3.3inch.png",width=3.3*ppi, height=2.5*ppi, res=ppi)
+options(scipen=2) # default: options(scipen=0)
+p3
+options(scipen=0) # default
+dev.off()
+
+ppi=300    # 300 is better
+png("plot_vol_group_AMY_20160313_300dpi_w3.3inch.png",width=3.3*ppi, height=2.5*ppi, res=ppi)
+p4
+dev.off()
+
+
+# --------------------------------------------------------------
+# plots of volumes between grops separated by hemi 4, 2016/03/14
+# --------------------------------------------------------------
+
+# - a code for error bar was changed. 2016/03/14. 
+# - This plots are trial for new appearances. 2016/03/14. 
+# - For the poster, the plots above were planed to be used. 2016/03/14. 
+
+
+# set up data
+
+datax=data.ex3.exna
+field.names=c("r.CC_Anterior", "r.CC_Mid_Anterior", "r.CC_Central", "r.CC_Mid_Posterior", "r.CC_Posterior")
+data.cc=jun.stack(field.names,c("caseid2","GROUP","ICV"))
+data.cc.central=subset(data.cc,ind=="r.CC_Central")
+field.names=c("r.Right.Lateral.Ventricle","r.Left.Lateral.Ventricle")
+data.lv=jun.stack(field.names,c("caseid2","GROUP","ICV"))
+field.names=c("r.Right.Inf.Lat.Vent","r.Left.Inf.Lat.Vent")
+data.lvt=jun.stack(field.names,c("caseid2","GROUP","ICV"))
+field.names=c("r.Right.Amygdala","r.Left.Amygdala")
+data.amy=jun.stack(field.names,c("caseid2","GROUP","ICV"))
+field.names=c("r.Right.Hippocampus","r.Left.Hippocampus")
+data.hip=jun.stack(field.names,c("caseid2","GROUP","ICV"))
+
+# function to plot 2016/02/24, 2016/03/09, 2016/03/14
+
+jun.plot32 <- function(datax,ylabel1,xlabel1,label1,posi1,posi2) {
+    datax$ind=factor(datax$ind)     # the order of the leves are not changed
+    #datax$GROUP=factor(datax$GROUP,rev(levels(datax$GROUP)))    # lever order to be PRO, HVPRO
+    datax$GROUP=factor(datax$GROUP)    # lever order to be HVPRO, PRO
+    n=length(levels(datax$ind));m=length(levels(datax$GROUP))
+    datax$R2D2=factor(    # factor(vector,levels) can explicitly express the order of the levels
+        paste(datax$ind,datax$GROUP,sep="."),     # example r.lt.amygdala.PRO
+        paste(levels(datax$ind)[rep(1:n,m)],levels(datax$GROUP)[sort(rep(1:m,n))],sep=".")    # hemi.Gr
+    )
+
+    ylabel=paste("Relative olumes of",ylabel1)    # combine tests for y label
+    p1=ggplot(datax, aes(x=R2D2,y=values,fill=GROUP)) +
+        theme_bw() + # change the thema to brack and white
+        theme(text=element_text(size=7)) +
+        scale_fill_manual(values=c("grey","red"),labels=c("HC","CHR")) +    # use default color if commented out
+        geom_dotplot(binaxis="y",stackdir="center") +
+        stat_summary(fun.ymin=function(x) mean(x), fun.ymax=function(x) mean(x), 
+            geom="errorbar", width=0.3) + # add mean as a error bar
+        #stat_summary(fun.ymin=function(x) mean(x) - sd(x), fun.ymax=function(x) mean(x) + sd(x), 
+        #    geom="errorbar", width=0.15) + # error bar
+        stat_summary(fun.data="mean_sdl", mult=1, geom="errorbar", width=0.15) + # error bar
+        #guides(fill=FALSE) +    # don't display guide
+        theme(legend.position=posi1,legend.justification=posi2) +    # this works 
+        theme(legend.background=element_blank()) +
+        theme(legend.key=element_blank()) +
+        theme(legend.key.size=unit(0.2, "cm")) +
+        theme(legend.text=element_text(size=7)) +  # no effect? 
+        guides(fill=guide_legend(title=NULL)) +
+        theme(axis.title.x=element_blank()) +    # don't display x-axis-label : GROUP
+        #xlab("HC                                     CHR") +    #  change x-axils-label
+        scale_x_discrete(breaks=c(levels(datax$R2D2)),
+            labels=c(xlabel1)) +   # change the labels of the scale
+        ylab(ylabel) +   # change the label of y axis
+        # scale_fill_discrete(labels=c("HC","CHR")) +   # This does not work 2016/03/10
+        annotate("text", x=-Inf, y=Inf, label=label1, hjust=-0.2, vjust=1.5)
+    p1
+}
+
+xlabel1=c("","")
+p1=jun.plot32(data.cc.central,"\nthe CCC",xlabel1,"A",c(1.05,1.1),c(1,1))
+xlabel1=c("left","right","left","right")
+p3=jun.plot32(data.lvt,"\nthe temporal horns of the LV",xlabel1,"B",c(0.05,1.1),c(0,1))
+p4=jun.plot32(data.amy,"\nthe amygdala",xlabel1,"C",c(1.05,1.1),c(1,1)) 
+options(scipen=2) # default: options(scipen=0)
+grid.arrange(p1, p3, p4)
+options(scipen=0) # default
+
+dev.copy2eps(file="plot_vol_group_20160311.eps",width=3.3, height=6)  # inches
+
+ppi=600    # 300 is better
+tiff("plot_vol_group_20160310.tif",width=3*ppi, height=6*ppi, res=ppi)
+options(scipen=2) # default: options(scipen=0)
+grid.arrange(p1, p3, p4)
+options(scipen=0) # default
+dev.off()
+
+# for poster
+
+ppi=300    # 300 is better
+png("plot_vol_group_CCC_20160313_300dpi_w3.3inch.png",width=3.3*ppi, height=2.5*ppi, res=ppi)
+p1
+dev.off()
+
+ppi=300    # 300 is better
+png("plot_vol_group_TH_20160313_300dpi_w3.3inch.png",width=3.3*ppi, height=2.5*ppi, res=ppi)
+options(scipen=2) # default: options(scipen=0)
+p3
+options(scipen=0) # default
+dev.off()
+
+ppi=300    # 300 is better
+png("plot_vol_group_AMY_20160313_300dpi_w3.3inch.png",width=3.3*ppi, height=2.5*ppi, res=ppi)
+p4
+dev.off()
 
 
 # ---------------------------------------------------------------------------------
@@ -495,15 +605,16 @@ tiff("plot_correlation_20160310.tif",width=3*ppi, height=6*ppi, res=ppi)
 grid.arrange(p2,p3,nrow=2)
 dev.off()
 
+# for poster
 
-# ppi=300    # 300 is better
-# png("plot_correlation_LV_20160309.png",width=8*ppi, height=8*ppi, res=ppi)
-# p2
-# dev.off()
-# 
-# ppi=300    # 300 is better
-# png("plot_correlation_HIP_20160309.png",width=8*ppi, height=8*ppi, res=ppi)
-# p3
-# dev.off()
+ppi=300    # 300 is better
+png("plot_correlation_LV_20160313_300ppi_w3.3inch.png",width=3.3*ppi, height=3.3*ppi, res=ppi)
+p2
+dev.off()
+
+ppi=300    # 300 is better
+png("plot_correlation_HIP_20160313_300ppi_w3.3inch.png",width=3.3*ppi, height=3.3*ppi, res=ppi)
+p3
+dev.off()
 
 

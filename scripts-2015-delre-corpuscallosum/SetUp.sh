4f8a1cec91eca2193f1a4f6ef16c075bb68d0599
niwatoritowani <niwatoritowani@example.com>
2016-04-04 17:58:27 -0400

    update

--- scripts-2015-delre-corpuscallosum/SetUp.sh
+++
@@ -1,8 +0,0 @@
-casedir=/projects/schiz/3Tdata/case${caseid}
-caseprojdir=${casedir}/projects/2015-delre-corpuscallosum
-ccdivroi=${caseprojdir}/${caseid}-cc-div-roi.nrrd
-ccroi=${caseprojdir}/${caseid}-cc-roi.nrrd
-outputdir=${caseprojdir}/${caseid}.cc_FW
-
-roicc_outdir=${outputdir}
-measured=${caseprojdir}/${caseid}.cc_FW_measured.txt

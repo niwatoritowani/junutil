071c7d82a1a91deaf1fc3d61ad763a932cc04580
niwatoritowani <niwatoritowani@users.noreply.github.com>
2016-04-27 00:12:31 -0400

    update

--- scripts-r/SetUpData_env_vaio.r
+++ scripts-r/SetUpData_env_vaio.r
@@ -8,7 +8,8 @@ library(car)    # for Anova()
 
 setwd("C:/Users/jun/Documents/test")
 setwd("02")  # 2015/10/27
-demographictable="Caselist_CC_prodromes.xlsx"
+#demographictable="Caselist_CC_prodromes.xlsx"    # commented out 2016/04/26
+demographictable="Caselist_CC_prodromes_anon_jun.xlsx" # 2016/04/26
 fsstatfile.aseg="edited.aseg_stats.txt"
 fsstatfile.rh="edited.aparc_stats_rh_volume.txt"
 fsstatfile.lh="edited.aparc_stats_lh_volume.txt"

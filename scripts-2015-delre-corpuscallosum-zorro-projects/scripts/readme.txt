403cbc6a125b8d8d0870e74eb8c1b6d88dd6e553
niwatoritowani <niwatoritowani@example.com>
2016-05-05 19:35:23 -0400

    update

---
+++ scripts-2015-delre-corpuscallosum-zorro-projects/scripts/readme.txt
@@ -0,0 +1,3 @@
+- FreeSurfer.qry was copied from /projects/schiz/software/LabPython/tract_querier/queries/FreeSurfer.qry
+- 2016/05/05
+
---
+++ scriptsWOupdate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dir_from=/rfanfs/pnl-zorro/projects/2015-delre-corpuscallosum
+file_copied="readme.md
+    scripts
+    SetUpData.sh
+    default.roicc.do
+"
+dir_to=/home/jkonishi/junutil/scripts-2015-delre-corpuscallosum-zorro-projects
+pushd ${dir_from} 
+pwd
+echo rsync -avu ${file_copied} ${dir_to}
+
